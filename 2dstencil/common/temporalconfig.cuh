5fdc199097d5d1f4434ed523a49ac38fa94158a5
cpwu <cpwu2017@126.com>
2025-10-13 16:01:25 +0800

    enable passing fusing steps through compile defination

--- 2dstencil/common/temporalconfig.cuh
+++ 2dstencil/common/temporalconfig.cuh
@@ -2,6 +2,10 @@
 
 #include "iptconfig.cuh"
 
+#ifndef TIMESTEMPS_HALO1_BOX_TILEY8_FLOAT
+#define TIMESTEMPS_HALO1_BOX_TILEY8_FLOAT 8
+#endif
+
 template<int halo, int shape, int ipt, class REAL>
 struct timesteps
 {
@@ -52,7 +56,8 @@ struct timesteps< 1,  box_shape,  4,  double>
 template<>
 struct timesteps< 1,  box_shape,  8,  float>
 {
-  static int const val = 8;//7;
+  // static int const val = 8;//7;
+  static int const val = TIMESTEMPS_HALO1_BOX_TILEY8_FLOAT;
   // static int const val = 1;//7;
 };
 
