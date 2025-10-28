5e9ad73c677d5fad63767723f9e6dedbec55de8c
cpwu <cpwu2017@126.com>
2025-10-28 13:27:45 +0800

    support more shape and dtype

--- 2dstencil/common/temporalconfig.cuh
+++ 2dstencil/common/temporalconfig.cuh
@@ -2,8 +2,20 @@
 
 #include "iptconfig.cuh"
 
-#ifndef TIMESTEMPS_HALO1_BOX_TILEY8_FLOAT
-#define TIMESTEMPS_HALO1_BOX_TILEY8_FLOAT 8
+#ifndef TIMESTEPS_HALO1_BOX_TILEY8_FLOAT
+#define TIMESTEPS_HALO1_BOX_TILEY8_FLOAT 8
+#endif
+
+#ifndef TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE
+#define TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE 4
+#endif
+
+#ifndef TIMESTEPS_HALO1_STAR_TILEY8_FLOAT
+#define TIMESTEPS_HALO1_STAR_TILEY8_FLOAT 8
+#endif
+
+#ifndef TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE
+#define TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE 4
 #endif
 
 template<int halo, int shape, int ipt, class REAL>
@@ -21,7 +33,8 @@ struct timesteps<halo,shape,ipt,double>
 template<>
 struct timesteps< 1,  star_shape,  4,  double>
 {
-  static int const val =3;
+  // static int const val =3;
+  static int const val = TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE;
 //static int const val =12;
 };
 
@@ -29,7 +42,8 @@ struct timesteps< 1,  star_shape,  4,  double>
 template<>
 struct timesteps< 1,  star_shape,  8,  float>
 {
-  static int const val = 9;
+  // static int const val = 9;
+  static int const val = TIMESTEPS_HALO1_STAR_TILEY8_FLOAT;
 };
 
 template<>
@@ -50,14 +64,15 @@ template<>
 struct timesteps< 1,  box_shape,  4,  double>
 {
   //static int const val = 6;
-  static int const val = 3;
+  // static int const val = 3;
+  static int const val = TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE;
 };
 
 template<>
 struct timesteps< 1,  box_shape,  8,  float>
 {
   // static int const val = 8;//7;
-  static int const val = TIMESTEMPS_HALO1_BOX_TILEY8_FLOAT;
+  static int const val = TIMESTEPS_HALO1_BOX_TILEY8_FLOAT;
   // static int const val = 1;//7;
 };
 
