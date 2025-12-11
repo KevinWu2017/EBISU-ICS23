9c6595564575e9e9128cc80ca14bffdaec9569ea
cpwu <cpwu2017@126.com>
2025-12-11 17:35:26 +0800

    support configurable steps for 2d and 3d shapes

--- 2dstencil/common/temporalconfig.cuh
+++ 2dstencil/common/temporalconfig.cuh
@@ -2,24 +2,53 @@
 
 #include "iptconfig.cuh"
 
+// Box2D1R Float
 #ifndef TIMESTEPS_HALO1_BOX_TILEY8_FLOAT
 #define TIMESTEPS_HALO1_BOX_TILEY8_FLOAT 8
 #endif
-
+// Box2D1R Double
 #ifndef TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE
 #define TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE 4
 #endif
-
+// Star2D1R Float
 #ifndef TIMESTEPS_HALO1_STAR_TILEY8_FLOAT
 #define TIMESTEPS_HALO1_STAR_TILEY8_FLOAT 8
 #endif
-
+// Star2D1R Double
 #ifndef TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE
 #define TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE 4
 #endif
-
+// Box2D2R Float
 #ifndef TIMESTEPS_HALO2_BOX_TILEY8_FLOAT
 #define TIMESTEPS_HALO2_BOX_TILEY8_FLOAT 6
+#endif
+// Box2D2R Double
+#ifndef TIMESTEPS_HALO2_BOX_TILEY4_DOUBLE
+#define TIMESTEPS_HALO2_BOX_TILEY4_DOUBLE 4
+#endif
+// Star2D2R Float
+#ifndef TIMESTEPS_HALO2_STAR_TILEY8_FLOAT
+#define TIMESTEPS_HALO2_STAR_TILEY8_FLOAT 8
+#endif
+// Star2D2R Double
+#ifndef TIMESTEPS_HALO2_STAR_TILEY4_DOUBLE
+#define TIMESTEPS_HALO2_STAR_TILEY4_DOUBLE 8
+#endif
+// Box2D3R Float
+#ifndef TIMESTEPS_HALO3_BOX_TILEY8_FLOAT
+#define TIMESTEPS_HALO3_BOX_TILEY8_FLOAT 1
+#endif
+// Box2D3R Double
+#ifndef TIMESTEPS_HALO3_BOX_TILEY4_DOUBLE
+#define TIMESTEPS_HALO3_BOX_TILEY4_DOUBLE 1
+#endif
+// Star2D3R Float
+#ifndef TIMESTEPS_HALO3_STAR_TILEY8_FLOAT
+#define TIMESTEPS_HALO3_STAR_TILEY8_FLOAT 1
+#endif
+// Star2D3R Double
+#ifndef TIMESTEPS_HALO3_STAR_TILEY4_DOUBLE
+#define TIMESTEPS_HALO3_STAR_TILEY4_DOUBLE 1
 #endif
 
 template<int halo, int shape, int ipt, class REAL>
@@ -34,6 +63,7 @@ struct timesteps<halo,shape,ipt,double>
   static int const val = 1;
 };
 
+// Star2D1R Double
 template<>
 struct timesteps< 1,  star_shape,  4,  double>
 {
@@ -42,7 +72,7 @@ struct timesteps< 1,  star_shape,  4,  double>
 //static int const val =12;
 };
 
-
+// Star2D1R Float
 template<>
 struct timesteps< 1,  star_shape,  8,  float>
 {
@@ -50,20 +80,39 @@ struct timesteps< 1,  star_shape,  8,  float>
   static int const val = TIMESTEPS_HALO1_STAR_TILEY8_FLOAT;
 };
 
+// Star2D2R Double
 template<>
 struct timesteps< 2,  star_shape,  4,  double>
 {
-  static int const val = 8;
+  // static int const val = 8;
+  static int const val = TIMESTEPS_HALO2_STAR_TILEY4_DOUBLE;
 };
 
-
+// Star2D2R Float
 template<>
 struct timesteps< 2,  star_shape,  8,  float>
 {
-  static int const val = 8;
+  // static int const val = 8;
+  static int const val = TIMESTEPS_HALO2_STAR_TILEY8_FLOAT;
 };
 
+// Star2D3R Double
+template<>
+struct timesteps< 3,  star_shape,  4,  double>
+{
+  // static int const val = 1;
+  static int const val = TIMESTEPS_HALO3_STAR_TILEY4_DOUBLE;
+};
 
+// Star2D3R Float
+template<>
+struct timesteps< 3,  star_shape,  8,  float>
+{
+  // static int const val = 1;
+  static int const val = TIMESTEPS_HALO3_STAR_TILEY8_FLOAT;
+};
+
+// Box2D1R Double
 template<>
 struct timesteps< 1,  box_shape,  4,  double>
 {
@@ -72,6 +121,7 @@ struct timesteps< 1,  box_shape,  4,  double>
   static int const val = TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE;
 };
 
+// Box2D1R Float
 template<>
 struct timesteps< 1,  box_shape,  8,  float>
 {
@@ -80,16 +130,35 @@ struct timesteps< 1,  box_shape,  8,  float>
   // static int const val = 1;//7;
 };
 
+// Box2D2R Double
 template<>
 struct timesteps< 2,  box_shape,  4,  double>
 {
-  static int const val = 4;
+  // static int const val = 4;
+  static int const val = TIMESTEPS_HALO2_BOX_TILEY4_DOUBLE;
 };
 
+// Box2D2R Float
 template<>
 struct timesteps< 2,  box_shape,  8,  float>
 {
   // static int const val = 6;//7;
   static int const val = TIMESTEPS_HALO2_BOX_TILEY8_FLOAT;
+};
+
+// Box2D3R Double
+template<>
+struct timesteps< 3,  box_shape,  4,  double>
+{
+  // static int const val = 1;
+  static int const val = TIMESTEPS_HALO3_BOX_TILEY4_DOUBLE;
+};
+
+// Box2D3R Float
+template<>
+struct timesteps< 3,  box_shape,  8,  float>
+{
+  // static int const val = 1;
+  static int const val = TIMESTEPS_HALO3_BOX_TILEY8_FLOAT;
 };
 
--- 3dstencil/common/temporalconfig.cuh
+++ 3dstencil/common/temporalconfig.cuh
@@ -14,6 +14,32 @@
 
 #include "iptconfig.cuh"
 
+// Box3D1R Float
+#ifndef TIMESTEPS_HALO1_BOX_TILEY8_FLOAT
+#define TIMESTEPS_HALO1_BOX_TILEY8_FLOAT 2
+#endif
+// Box3D1R Double
+#ifndef TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE
+#define TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE 1
+#endif
+// Star3D1R Float
+#ifndef TIMESTEPS_HALO1_STAR_TILEY8_FLOAT
+#define TIMESTEPS_HALO1_STAR_TILEY8_FLOAT 8
+#endif
+// Star3D1R Double
+#ifndef TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE
+#define TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE 3
+#endif
+
+// Star3D2R Float
+#ifndef TIMESTEPS_HALO2_STAR_TILEY8_FLOAT
+#define TIMESTEPS_HALO2_STAR_TILEY8_FLOAT 5
+#endif
+// Star3D2R Double
+#ifndef TIMESTEPS_HALO2_STAR_TILEY4_DOUBLE
+#define TIMESTEPS_HALO2_STAR_TILEY4_DOUBLE 5
+#endif
+
 
 template<int halo, int shape, int ipt, class REAL>
 struct timesteps
@@ -28,48 +54,59 @@ struct timesteps<halo,shape,ipt,double>
   static int const val = 1;
 };
 
+// Star3D1R Double
 template<>
 struct timesteps< 1,  star_shape,  4,  double>
 {
   // static int const val = 1;
-  static int const val = 3;
+  // static int const val = 3;
   // static int const val = 8;
+  static int const val = TIMESTEPS_HALO1_STAR_TILEY4_DOUBLE;
 };
 
+// Star3D1R Float
 template<>
 struct timesteps< 1,  star_shape,  8,  float>
 {
-  static int const val = 8;
+  // static int const val = 8;
   // static int const val = 2;
+  static int const val = TIMESTEPS_HALO1_STAR_TILEY8_FLOAT;
 };
 
 
-
+// Star3D2R Double
 template<>
 struct timesteps< 2,  star_shape,  4,  double>
 {
-  static int const val = 5;
+  // static int const val = 5;
+  static int const val = TIMESTEPS_HALO2_STAR_TILEY4_DOUBLE;
 };
 
+// Star3D2R Float
 template<>
 struct timesteps< 2,  star_shape,  8,  float>
 {
-  static int const val = 5;
+  // static int const val = 5;
+  static int const val = TIMESTEPS_HALO2_STAR_TILEY8_FLOAT;
 };
 
+// Box3D1R Double
 template<>
 struct timesteps<1, box_shape,  4,  double>
 {
   // static int const val = 5;
   // static int const val = 3;
   // static int const val = 1;
-  static int const val = 1;
+  // static int const val = 1;
+  static int const val = TIMESTEPS_HALO1_BOX_TILEY4_DOUBLE;
 };
 
+// Box3D1R Float
 template<>
 struct timesteps< 1,  box_shape,  8,  float>
 {
-  static int const val = 2;
+  // static int const val = 2;
+  static int const val = TIMESTEPS_HALO1_BOX_TILEY8_FLOAT;
 };
 
 template<>
